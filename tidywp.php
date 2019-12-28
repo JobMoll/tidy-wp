73d85b31d44686122a0455ce09ac5a045277cc6e
Job Moll <jobmoll@gmail.com>
2019-12-28 18:27:29 +0100

    Version change

--- tidywp.php
+++ tidywp.php
@@ -4,7 +4,7 @@
 Plugin Name: Tidy WP
 Plugin URI: https://tidywp.com/
 Description: A clean & easy way to manage multiple Wordpress websites! This plugin is needed to get the Tidy WP app working!
-Version: 0.0.1
+Version: 0.0.2
 Author:            Job Moll
 Author URI:        https://sparknowmedia.com
 License: GPL-3.0
