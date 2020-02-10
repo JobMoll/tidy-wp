0e101a9795ff2747bba790ef875d9c033a2f6ff6
Job Moll <jobmoll@gmail.com>
2020-02-10 21:19:08 +0100

    Site settings fix

--- includes/class-site-settings.php
+++ includes/class-site-settings.php
@@ -3,46 +3,49 @@
 function site_settings($data) {
    if (intval(get_option('tidywp_brute_force_check')) <= 3) {
     if (isset($_SERVER['HTTP_TOKEN'])) {
-     if (($_SERVER['HTTP_TOKEN'] == $GLOBALS['secretToken']) && (in_array(encrypt_and_decrypt($_SERVER['LOGGEDIN_USERNAME'], 'e' ), $GLOBALS['$usernameArray']))) {
+	$arrayHeaderHTTP = explode(',', $_SERVER['HTTP_TOKEN']);
+     if (($arrayHeaderHTTP[0] == $GLOBALS['secretToken']) && (in_array(encrypt_and_decrypt($arrayHeaderHTTP[1], 'e' ), $GLOBALS['usernameArray']))) {
         
-        if ($data->get_param('siteTitle') != '' && !empty($data->get_param('siteTitle'))) {
+        if ($data->get_param('siteTitle') != '') {
             update_option('blogname', $data->get_param('siteTitle'), 'yes' );
         } 
-        if ($data->get_param('tagline') != '' && !empty($data->get_param('tagline'))) {
+        if ($data->get_param('tagline') != '') {
             update_option('blogdescription', $data->get_param('tagline'), 'yes' );
         }
         
         // value 0 is disabled --- 1 is enabled
-        if ($data->get_param('usersCanRegister') != '' && !empty($data->get_param('usersCanRegister'))) {
+        if ($data->get_param('usersCanRegister') != '') {
             update_option('users_can_register', $data->get_param('usersCanRegister'), 'yes' );
         }
         
         // value 0 is search engine visibility on --- 1 is off
-        if ($data->get_param('blogPublic') != '' && !empty($data->get_param('blogPublic'))) {
+       if ($data->get_param('blogPublic') != '') {
             update_option('blog_public', $data->get_param('blogPublic'), 'yes' );
-        }
+       }
+	
 
         // send the redirect URL and which redirect type
-        if ($data->get_param('redirectWebsiteURL') != '' && !empty($data->get_param('redirectWebsiteURL')) && $data->get_param('redirectWebsiteType') != '' && !empty($data->get_param('redirectWebsiteType'))) {
+        if ($data->get_param('redirectWebsiteURL') != '' && $data->get_param('redirectWebsiteType') != '') {
             update_option('tidywp_redirect_website_url', $data->get_param('redirectWebsiteURL'), 'no' );
             update_option('tidywp_redirect_type', $data->get_param('redirectWebsiteType'), 'no' );
         } 
         // set to true to empty the option
-        if ($data->get_param('redirectWebsiteURLDisable') != '' && !empty($data->get_param('redirectWebsiteURLDisable'))) {
+        if ($data->get_param('redirectWebsiteURLDisable') != '') {
             update_option('tidywp_redirect_website_url', '', 'no' );
             update_option('tidywp_redirect_type', '', 'no' );
         }
 
-if ($data->get_param('show') == 'true' && !empty($data->get_param('show'))) {
-echo '{"SiteTitle":"' . get_option('blogname') . '", ';
 
+echo '{"SiteTitle":"' . get_option('blogname') . '", ';
 echo '"Tagline":"' . get_option('blogdescription') . '", ';
 echo '"UsersCanRegister":"' . get_option('users_can_register') . '", ';
 echo '"RedirectWebsiteURL":"' . get_option('tidywp_redirect_website_url') . '", ';
 echo '"RedirectWebsiteType":"' . get_option('tidywp_redirect_type') . '", ';
 echo '"BlogPublic":"' . get_option('blog_public') . '"}';
-}
+
    
+     } else { 
+echo 'Sorry... you are not allowed to view this data.';
 }
 } else {
 echo 'Sorry... you are not allowed to view this data.';
@@ -58,7 +61,7 @@ resetTokenAndPath();
 
 update_option('tidywp_brute_force_check', '0', 'no' );
 }
-} 
+}  
  
 // add to rest api
 add_action( 'rest_api_init', function () {
