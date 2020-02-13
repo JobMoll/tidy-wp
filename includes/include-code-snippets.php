a6eb3558dd659ddab69f08bc8d8d5598d6e2fb27
Job Moll <jobmoll@gmail.com>
2020-02-13 22:02:55 +0100

    Added way more security for smart security option

--- includes/include-code-snippets.php
+++ includes/include-code-snippets.php
@@ -57,7 +57,7 @@ if ( ! class_exists( 'Limit_Login_Attempts' ) ) {
                     $until = get_option( '_transient_timeout_' . $this->transient_name );
                     $time = $this->when( $until );
 
-                    //Display error message to the user when limit is reached 
+                    //Display error message to the user when limit is reached
                     return new WP_Error( 'too_many_tried', sprintf( __( '<strong>ERROR</strong>: You have reached authentification limit, you will be able to try again in %1$s.' ) , $time ) );
                 }
             }
@@ -154,7 +154,7 @@ function remove_version_from_scripts( $src ) {
 }
 add_filter( 'style_loader_src', 'remove_version_from_scripts');
 add_filter( 'script_loader_src', 'remove_version_from_scripts');
-remove_action('wp_head', 'wp_generator'); 
+remove_action('wp_head', 'wp_generator');
 
 
 // HTTPResponseSecurity
@@ -193,6 +193,91 @@ error_reporting(0);
 
 // disable plugin and theme editor
 define( 'DISALLOW_FILE_EDIT', true );
+    
+//Disable trackbacks and pingbacks
+function filterPostComments($posts) {
+    foreach ($posts as $key => $p) {
+        if ($p->comment_count <= 0) { return $posts; }
+        $comments = get_approved_comments((int)$p->ID);
+        $comments = array_filter($comments, "stripTrackback");
+        $posts[$key]->comment_count = sizeof($comments);
+    }
+    return $posts;
+}
+//Updates the count for comments and trackbacks
+function filterTrackbacks($comms) {
+global $comments, $trackbacks;
+    $comments = array_filter($comms,"stripTrackback");
+    return $comments;
+}
+//Strips out trackbacks/pingbacks
+function stripTrackback($var) {
+    if ($var->comment_type == 'trackback' || $var->comment_type == 'pingback') { return false; }
+    return true;
+}
+  
+add_filter('comments_array', 'filterTrackbacks', 0);
+add_filter('the_posts', 'filterPostComments', 0);
+    
+// Prevent user enumeration
+if (!is_admin()) {
+    // default URL format
+    if (preg_match('/author=([0-9]*)/i', $_SERVER['QUERY_STRING'])) die();
+    add_filter('redirect_canonical', 'shapeSpace_check_enum', 10, 2);
+}
+function shapeSpace_check_enum($redirect, $request) {
+    // permalink URL format
+    if (preg_match('/\?author=([0-9]*)(\/*)/i', $request)) die();
+    else return $redirect;
+}
+    
+//
+// Disable use XML-RPC
+add_filter( 'xmlrpc_enabled', '__return_false' );
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+    
+// Disable X-Pingback to header
+add_filter( 'wp_headers', 'disable_x_pingback' );
+function disable_x_pingback( $headers ) {
+    unset( $headers['X-Pingback'] );
+
+return $headers;
+}
+
+// Disable Emojicons tinymce
+function disable_wp_emojicons() {
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+    add_filter( 'emoji_svg_url', '__return_false' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
+
+function disable_emojicons_tinymce( $plugins ) {
+    return is_array( $plugins ) ? array_diff( $plugins, array( 'wpemoji' ) ) : array();
+}
+
+// Disable the message - JQMIGRATE: Migrate is installed, version 1.4.1
+add_action('wp_default_scripts', function ($scripts) {
+    if (!empty($scripts->registered['jquery'])) {
+        $scripts->registered['jquery']->deps = array_diff($scripts->registered['jquery']->deps, ['jquery-migrate']);
+    }
+});
+    
+// Disable the WP REST API
+add_filter('rest_enabled', '__return_false');
+add_filter('rest_jsonp_enabled', '__return_false');
+add_filter('json_enabled', '__return_false');
+    
+remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'template_redirect', 'rest_output_link_header', 11 );
 }
 
 
@@ -203,14 +288,14 @@ function redirect_to_nonexistent_page(){
      $new_login=  get_option('tidywp_hide_login');
     if(strpos($_SERVER['REQUEST_URI'], $new_login) === false){
                 wp_safe_redirect( home_url( 'NonExistentPage' ), 302 );
-      exit(); 
+      exit();
     }
  }
 add_action( 'login_head', 'redirect_to_nonexistent_page');
 
 function redirect_to_actual_login(){
   $new_login =  get_option('tidywp_hide_login');
-  if(parse_url($_SERVER['REQUEST_URI'],PHP_URL_QUERY) == $new_login&& ($_GET['redirect'] !== false)){     
+  if(parse_url($_SERVER['REQUEST_URI'],PHP_URL_QUERY) == $new_login&& ($_GET['redirect'] !== false)){
                  wp_safe_redirect(home_url("wp-login.php?$new_login&redirect=false"));
      exit();
  
